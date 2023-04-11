16d1fdd245e4f17246fa41218db43cee7afcbd9d
reddydiwu <83949294+reddydiwu@users.noreply.github.com>
2023-04-11 12:55:51 +0530

    Add files via upload

---
+++ Shop_Vibes/Shop_Vibes/ShopVibes/src/main/java/com/project/ShopVibes/ShopVibesApplication.java
@@ -0,0 +1,13 @@
+package com.project.ShopVibes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopVibesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopVibesApplication.class, args);
+	}
+
+}
